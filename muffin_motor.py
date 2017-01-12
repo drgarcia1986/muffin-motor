b885bd87ef5d07dae52e3a192d03df42c423c155
Diego Garcia <drgarcia1986@gmail.com>
2017-01-12 14:04:30 -0200

    Bump version: 0.2.0 → 0.3.0

--- muffin_motor.py
+++ muffin_motor.py
@@ -4,7 +4,7 @@ import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
 from muffin.plugins import BasePlugin
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __project__ = "muffin-motor"
 __author__ = "Diego Garcia <drgarcia1986@gmail.com>"
 __license__ = "MIT"
